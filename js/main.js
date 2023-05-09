461a89e297091ff827daa1990103a10798b3bf5c
hijunglit <hijunglit.gmail.com>
2023-05-09 10:53:14 +0900

    code upload from local to master

---
+++ js/main.js
@@ -0,0 +1,84 @@
+window.addEventListener("DOMContentLoaded", paintClock);
+
+function paintClock() {
+    const date = new Date();
+    const container = document.querySelector(".clock-container");
+    const hours = document.querySelector(".hours");
+    const minutes = document.querySelector(".minutes");
+    const seconds = document.querySelector(".seconds"); 
+    const getHours = String(date.getHours()).padStart(2,"0");
+    const getMinutes = String(date.getMinutes()).padStart(2,"0");
+    const getSeconds = String(date.getSeconds()).padStart(2,"0");
+    container.innerText = `${getHours}:${getMinutes}:${getSeconds}`;
+}
+workClock();
+function workClock() {
+    setInterval(paintClock, 1000);
+}
+
+/* add grocery */
+const form = document.querySelector("#form");
+const groceryList = document.querySelector(".list");
+let items;
+
+// event listener
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+    const input = form.querySelector("input#add");
+    const date = new Date();
+    let itemId = date.getTime();
+    const itemObj = {id:`${itemId}`, item:`${input.value}`}
+    items.push(itemObj);
+    localStorage.setItem("items", JSON.stringify(items));
+    displayItems();
+    input.value = '';
+}
+
+function displayItems() {
+    const input = form.querySelector("input#add");
+    const savedItems = JSON.parse(localStorage.getItem("items"));
+    groceryList.innerText = "";
+    if(!savedItems) {
+        items = [];
+    } else {
+        for(let i = 0; i<savedItems.length; i++) {
+            let li = document.createElement("li");
+            li.innerHTML = `<span>${savedItems[i].item}</span><span class="delete">❌</span>`;
+            li.id = savedItems[i].id;
+            groceryList.append(li);
+        }
+        // Remove grocery
+        let deleteBtn = groceryList.querySelectorAll("li span.delete");
+        deleteBtn.forEach((item) => {
+            item.addEventListener("click", function() {
+                item.parentNode.remove();
+                const targetItem = items.find((element) => element.id == item.parentNode.id);
+                const targetIndex = items.indexOf(targetItem);
+                items.splice(targetIndex, 1);
+                localStorage.setItem("items", JSON.stringify(items));
+            });
+        });
+        items = savedItems;
+    }
+
+    /*********** When you use forEach function instead of for statement **************/
+    // savedItems.forEach((item, index) => {
+    //     let li = document.createElement("li");
+    //     li.innerText = item.item;
+    //     groceryList.append(li);
+    // });
+}
+
+displayItems();
+
+// Update grocery
+// let editBtn = document.querySelectorAll("li span.edit");
+// editBtn.forEach((item) => {
+//     item.addEventListener("click", function() {
+//         input.value = item.previousSibling.innerHTML;
+//         const targetItem = item.previousSibling;
+//         console.log(targetItem);
+//     });
+// });
